f83fcbbaf98a726868fef19142b8f81c4058f2fa
Mateusz <mateuszrustowiczyt@gmail.com>
2023-06-18 21:25:00 +0200

    my first commit

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ async function connectToDb() {
 connectToDb();
 
 dotenv.config();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 
